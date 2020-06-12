7649988cdca32483af35ab91920ea38513492627
sgsettle <s.settle41@gmail.com>
2020-06-12 11:23:54 -0400

    update

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     // this is the local host name of react app
     case 'localhost' || '127.0.0.1':
     // this is the local host name of your server/API
-    APIURL = 'http://localhost:3000';
-    break;
+        APIURL = 'http://localhost:3000';
+        break;
     // this is the deployed react app
     case 'sgs-hungryhound.herokuapp.com':
         // this is the full url of deployed server/API
